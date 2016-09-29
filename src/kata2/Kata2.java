4a0f884e080e2f3766c038c85b82f34a57236ab0
usuario <usuario@10.22.143.52>
2016-09-29 12:00:07 +0100

    kata2: version 3.0

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -2,30 +2,21 @@ package kata2;
 import java.util.HashMap;
 import java.util.Map;
 
-//version 2.0
+//version 3.0
 public class Kata2 {
     public static void main(String[] args) {
         
-        int[] data = {1,1,1,100,2,4,5,6,8,8,4,5,6,2,-4};
-        
-        //array asociativo histograma (clave entera, valor entero)
-        Map<Integer,Integer> histogram = new HashMap<>();
-        
+        //Integer[] data = {1,1,1,100,2,4,5,6,8,8,4,5,6,2,-4};
+        String [] data = {"Ana", "Juan", "Juan","Juan" , "Ana", "Rosa"};
+        Histogram histo = new Histogram(data);
+        Map<String,Integer> histogr = histo.getHistogram();
         //cambiamos el for por un for del tipo (fore+Tab) con operador ternario
         
-        //este tipo de bucle, guarda el valor de la iteracion n del array data 
-        //en value
-        
-        for (int value : data) {
-            
-            //operador ternario (condicion) ? valor1:valor2; si la condicion es
-            //true se cumple el valor 1 si es false el valor 2
-            
-            histogram.put(value, histogram.containsKey(value) ? histogram.get(value)+1 : 1);
-        }
+
         //atajazo 2 fore + TAB
-        for (int key : histogram.keySet()) {
-            System.out.println(key + "-->" + histogram.get(key));
+        
+        for (String key : histogr.keySet()) {
+            System.out.println(key + "-->" + histogr.get(key));
         }
     }
 }
